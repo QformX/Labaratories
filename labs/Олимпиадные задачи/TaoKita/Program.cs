c35e845ce4db8bf71ca212be324c345c0221bb28
QformX <101177599+QformX@users.noreply.github.com>
2023-02-20 13:13:08 +0100

    Add files via upload

---
+++ labs/Олимпиадные задачи/TaoKita/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace TaoKita
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(NormalizeText(Console.ReadLine().Split(' ')));
+            Console.ReadKey();
+        }
+
+        static string NormalizeText(string[] text)
+        {
+            string[] tmp = new string[text.Length];
+            int count = text.Length / 2;
+            for (int i = 0; i < text.Length; i++)
+            {
+                count += Convert.ToInt32(Math.Pow(-1.0, i) * i);
+                tmp[i] = NormalizeWord(text[count]);
+            }
+            return String.Join(" ", tmp);
+        }
+
+        static string NormalizeWord(string word)
+        {
+            char[] tmp = new char[word.Length];
+            int count = word.Length / 2;
+            for (int i = 0; i < word.Length; i++)
+            {
+                count += Convert.ToInt32(Math.Pow(-1.0, i) * i);
+                tmp[i] = word[count];
+            }
+            return new string(tmp);
+        }
+    }
+}
