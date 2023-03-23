b3f7b427413728e60d3bbad19eae2867919486a8
QformX <101177599+QformX@users.noreply.github.com>
2023-03-23 13:23:16 +0600

    Add files via upload

---
+++ labs/Аудиторные лабы/Stack 23.03.23/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication46
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine().Replace(" ", "");
+
+            if (IsQueueRight(text))
+            {
+                Console.WriteLine("Прав"); 
+            }
+            else
+            {
+                Console.WriteLine("НеПрав"); 
+            }
+
+            Console.ReadLine();
+        }
+
+        private static bool IsQueueRight(string text)
+        {
+            Stack<char> que_void = new Stack<char>();
+
+            bool ans = true;
+
+            foreach (var item in text)
+            {
+                if (item == '{' || item == '(' || item == '[')
+                {
+                    que_void.Push(item);
+                }
+                else if (que_void.Count() != 0)
+                {
+                    if (item == '}' && que_void.Peek() != '{')
+                    {
+                        return ans = false;
+                    }
+                    else if (item == ']' && que_void.Peek() != '[')
+                    {
+                        return ans = false;
+                    }
+                    else if (item == ')' && que_void.Peek() != '(')
+                    {
+                        return ans = false;
+                    }
+                    else
+                    {
+                        que_void.Pop();
+                    }
+                }
+                else
+                {
+                    return ans = false;
+                }
+            }
+            return ans;
+        }
+
+        static void Print(Stack<char> que)
+        {
+            foreach (var item in que)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
