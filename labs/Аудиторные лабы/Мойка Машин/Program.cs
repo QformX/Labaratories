4b7b4d78f1335813b1e446458de6ac3147f5a843
QformX <101177599+QformX@users.noreply.github.com>
2023-05-04 08:26:06 +0100

    Add files via upload

---
+++ labs/Аудиторные лабы/Мойка Машин/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        delegate void delWash(Garage gar);
+
+        static void Main()
+        {
+            delWash washing = Washer.Wash;
+
+            Console.WriteLine("VVEDITE KOL-VO MASHIN");
+            int n = int.Parse(Console.ReadLine());
+
+            Garage gar = new Garage(n);
+
+            washing(gar);
+            
+            Console.ReadLine();
+        }
+    }
+
+    class Car
+    {
+        string Mark;
+        string Sign;
+
+        public Car(string Mark = "Unmarked", string Sign = "Unsigned")
+        {
+            this.Mark = Mark;
+            this.Sign = Sign;
+        }
+
+        public string GetMark()
+        {
+            return Mark;
+        }
+    }
+
+    class Washer
+    {
+        List<Car> cars;
+
+        public Washer(List<Car> cars)
+        {
+            this.cars = cars;
+        }
+
+        public static void Wash(Garage gar)
+        {
+            for (int i = 0; i < gar.GetLength(); i++)
+            {
+                Car car = gar.TakeCar();
+                Console.WriteLine("Машина {0} пoмыта!!!!", car.GetMark());
+                gar.ReturnCar(car);  
+            }
+        }
+    }
+
+    class Garage
+    {
+        Queue<Car> gc_cars = new Queue<Car>();
+
+        public Garage(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("MARKA: ");
+                string mark = Console.ReadLine();
+                Console.Write("NOMER: ");
+                string sign = Console.ReadLine();
+
+                Car car = new Car(mark, sign);
+
+                gc_cars.Enqueue(car);
+            }
+
+            Console.WriteLine("GARAJ ZAPOLNEN");
+        }
+
+        public Car TakeCar()
+        {
+            return gc_cars.Dequeue();
+        }
+
+        public void ReturnCar(Car car)
+        {
+            gc_cars.Enqueue(car);
+        }
+        
+        public int GetLength()
+        {
+            return gc_cars.Count;
+        }
+    }
+}
---
+++ labs/Аудиторные лабы/Обобщённый массив/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main()
+        {
+            GenArray<string> strArr = new GenArray<string>(4);
+            GenArray<int> intArr = new GenArray<int>(4);
+            GenArray<bool> boolArr = new GenArray<bool>(4);
+
+            for (int i = 0; i < 5; i++)
+            {
+                strArr.Add("Привет");
+                intArr.Add(5);
+                boolArr.Add(true);
+            }
+
+            strArr.Delete();
+            strArr.Delete();
+
+            boolArr.Delete();
+
+            Console.WriteLine(intArr.GetByIndex(3));
+
+            strArr.Show();
+            intArr.Show();
+            boolArr.Show();
+
+            Console.ReadKey();
+
+
+        }
+    }
+
+    class GenArray<T>
+    {
+        int n = 0;
+        int count = 0;
+        int countOfAdds = 0;
+        List<T> list;
+        
+        public GenArray(int n)
+        {
+            this.n = n;
+            this.count = n - 1;
+            list = new List<T>(n);
+        }
+
+        public void Add(T value)
+        {
+            if (countOfAdds < n)
+            {
+                list.Add(value);
+                countOfAdds++;
+            }
+            else Console.WriteLine("Больше низя");
+        }
+
+        public void Delete()
+        {
+            if (count >= 0)
+            {
+            list[count] = default(T);
+            count--;
+            }
+        }
+
+        public T GetByIndex(int index)
+        {
+            if (index <= n) return list[index];
+
+            return default(T);
+        }
+
+        public int GetLength()
+        {
+            return n;
+        }
+
+        public void Show()
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
