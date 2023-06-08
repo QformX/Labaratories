c203f18352764cb962e258813b59c045e6975dfd
QformX <101177599+QformX@users.noreply.github.com>
2023-06-08 18:33:12 +0600

    Add files via upload

---
+++ labs/Олимпиадные задачи/Apple_Tree/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace AppleTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader Inp = new StreamReader("Input.txt"))
+            {
+                string[] line = Inp.ReadLine().Split();
+                int N = int.Parse(line[0]), M = int.Parse(line[1]);
+                int[] C = new int[M], S = new int[M];
+                int[,] tree = new int[N + 1, N + 1];
+                for (int i = 0; i < N + 1; i++)
+                {
+                    for (int j = 0; j < N + 1; j++)
+                    {
+                        tree[i, j] = (i != j) ? int.MaxValue : 0;
+                    }
+                }
+                for (int i = 0; i < N; i++)
+                {
+                    line = Inp.ReadLine().Split();
+                    int K = int.Parse(line[0]), L = int.Parse(line[1]);
+                    tree[K, i + 1] = tree[i + 1, K] = L;
+                }
+                for (int j = 0; j < M; j++)
+                {
+                    line = Inp.ReadLine().Split();
+                    C[j] = int.Parse(line[0]);
+                    S[j] = int.Parse(line[1]);
+                }
+                line = Inp.ReadLine().Split();
+                int X = int.Parse(line[0]), Z = int.Parse(line[1]);
+                for (int k = 0; k < N + 1; k++)
+                {
+                    for (int i = 0; i < N + 1; i++)
+                    {
+                        for (int j = 0; j < N + 1; j++)
+                        {
+                            tree[i, j] = Math.Min(tree[i, k] + tree[k, j], tree[i, j]);
+                        }
+                    }
+                }
+                for (int x = M - 1; x >= 0; x--)
+                {
+                    if (S[x] < Z)
+                    {
+                        C = C.Where((val, idx) => idx != x).ToArray();
+                    }
+                }
+                int ans = 0;
+                while (C.Length > 0)
+                {
+                    int[] aim = C.Select(c => new int[] { tree[X, c], c }).OrderBy(a => a[0]).First();
+                    ans += aim[0];
+                    X = aim[1];
+                    C = C.Where(c => c != aim[1]).ToArray();
+                }
+                using (StreamWriter Outp = new StreamWriter("Output.txt"))
+                {
+                    Outp.Write(ans);
+                }
+            }
+        }
+    }
+}
---
+++ labs/Олимпиадные задачи/Potion_Making/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace PotionMaking
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var actions = new List<string>();
+            using (var sr = new StreamReader("input.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    actions.Add(line);
+                }
+            }
+
+            var ingr = new List<List<string>>();
+            foreach (var action in actions)
+            {
+                var parts = action.Split();
+                ingr.Add(parts.Skip(1).ToList());
+            }
+
+            var spell = "";
+            var cast = new List<string>();
+            var potion = new List<string>();
+
+            for (var i = 0; i < actions.Count; i++)
+            {
+                var parts = actions[i].Split();
+                for (var j = 0; j < ingr[i].Count; j++)
+                {
+                    if (int.TryParse(ingr[i][j], out int castInd))
+                    {
+                        if (potion.Count < castInd)
+                        {
+                            potion.RemoveAt(castInd - 1);
+                        }
+                        ingr[i][j] = cast[castInd - 1];
+                    }
+                }
+
+                if (parts[0] == "MIX")
+                {
+                    spell += "MX" + string.Join("", ingr[i]) + "XM";
+                }
+                else if (parts[0] == "WATER")
+                {
+                    spell += "WT" + string.Join("", ingr[i]) + "TW";
+                }
+                else if (parts[0] == "DUST")
+                {
+                    spell += "DT" + string.Join("", ingr[i]) + "TD";
+                }
+                else if (parts[0] == "FIRE")
+                {
+                    spell += "FR" + string.Join("", ingr[i]) + "RF";
+                }
+
+                cast.Add(spell);
+                potion.Add(spell);
+                spell = "";
+            }
+
+            Console.WriteLine(potion[potion.Count - 1]);
+
+            using (StreamReader sr = new StreamReader("output.txt"))
+            {
+                string answer = sr.ReadToEnd();
+                if (potion[potion.Count - 1] == answer)
+                {
+                    Console.WriteLine("Успешно");
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
